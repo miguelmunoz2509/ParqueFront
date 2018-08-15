f84944782ce7303e9787ab1ef19ccb1ea976805a
Miguel Muñoz <Miguel.Muoz@iteria.com.co>
2018-08-15 13:10:52 -0500

    primer commit

---
+++ src/main/java/co/com/parque/front/view/ListaParqueView.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.parque.front.view;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Usuario
+ */
+@ManagedBean
+@ViewScoped
+public class ListaParqueView {
+    public String consultarParque(){
+        System.out.println("mamo");
+        return "";
+    }
+}
