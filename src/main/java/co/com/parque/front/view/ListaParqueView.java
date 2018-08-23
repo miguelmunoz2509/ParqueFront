cb0f8e4f45adfe8926411d7485f3770cac18d2cd
Miguel Muñoz <Miguel.Muoz@iteria.com.co>
2018-08-23 13:33:29 -0500

    Creacion de clase ListaParqueView en paquete view. Creacion de metodo de consulta parques, en el cual se crea el cliente, se consume la url y en una lista se almacenan los objetos json

--- src/main/java/co/com/parque/front/view/ListaParqueView.java
+++ src/main/java/co/com/parque/front/view/ListaParqueView.java
@@ -5,8 +5,18 @@
  */
 package co.com.parque.front.view;
 
+import co.com.parque.front.model.Parque;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 
 /**
  *
@@ -15,9 +25,38 @@ import javax.faces.bean.ViewScoped;
 @ManagedBean
 @ViewScoped
 public class ListaParqueView {
-    public String consultarParque(){
-        System.out.println("mamo");
-        return "";
+    private List<Parque>  listaparques = new ArrayList<>();
+    private final String url = "https://private-05017d-parques1.apiary-mock.com/parks?status=";
+    
+    //getters y setters
+
+    public List<Parque> getListaparques() {
+        return listaparques;
+    }
+
+    public void setListaparques(List<Parque> listaparques) {
+        this.listaparques = listaparques;
     }
     
+    
+    public String consultaParques(){
+        Client cliente = ClientBuilder.newClient();                                 
+        WebTarget rs =cliente.target(url);       
+        JsonArray jsonarray = (JsonArray) rs.request(MediaType.APPLICATION_JSON).get(JsonArray.class);
+        
+        Iterator iter = jsonarray.iterator();
+        while(iter.hasNext()){
+            JsonObject val = (JsonObject)iter.next();                 
+            listaparques.add(Parque.fromJson(val));           
+            /*
+            equipoTemp.setCodigo(val.getString("codigo"));
+            equipoTemp.setNombre(val.getString("nombre"));
+            equipoTemp.setColor(val.getString("Naranja"));
+            equipoTemp.setDt(val.getString("dt"));
+            equipoTemp.setDivision(val.getString("fechaFund"));
+            equipoTemp.setFecha_fundacion(val.getString("division"));
+             */      
+        }
+        return "";
+    }
 }
